232fcbd2f264aad6d883b9bc40db2ba383ba0f93
Timur <tima.temirov.2005@mail.ru>
2025-01-26 14:47:00 +0300

    Task model + crud.

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import telebot
 from dotenv import load_dotenv
 import logging
 
+from models import create_task, get_task, get_all_tasks, update_task
+
 
 def main():
-    load_dotenv()
-    token = os.environ.get('TELEGRAM_TOKEN')
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s [%(levelname)s]: %(message)s',
         handlers=[logging.FileHandler('bot.log', 'w', 'utf-8')]
     )
+    load_dotenv()
+    token = os.environ.get('TELEGRAM_TOKEN')
+    logging.info('Loaded api key.')
     bot = telebot.TeleBot(token)
-    logging.info('Bot started')
+    logging.info('Bot started.')
+
+
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+        logging.info('Received /start command.')
+        tasks = get_all_tasks()
+        bot.send_message(message.chat.id, 'Привет, ты написал мне /start\nСписок задач:')
+        for task in tasks:
+            bot.send_message(message.chat.id, task.title)
 
     bot.polling(none_stop=True, interval=0)
 
---
+++ models.py
@@ -0,0 +1,84 @@
+import datetime
+import logging
+from typing import List, Type, Optional
+from sqlalchemy import Integer, Column, String, Boolean, DateTime, create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
+
+
+class Base(DeclarativeBase): pass
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    done = Column(Boolean, default=False)
+    start_time = Column(DateTime, default=datetime.datetime.now)
+    end_time = Column(DateTime, nullable=True, default=None)
+
+    def open(self):
+        update_task(self.id, done=False)
+        logging.info(f'task {self.id} opened.')
+
+    def close(self):
+        update_task(self.id, done=True)
+        logging.info(f'task {self.id} closed.')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
+engine = create_engine('sqlite:///db.sqlite3')
+Base.metadata.create_all(engine)
+
+def create_task(title: str, description: str, start_time: datetime.datetime = None, end_time: datetime.datetime = None):
+    with Session(autoflush=False, bind=engine) as db:
+        task = Task(
+            title=title,
+            description=description,
+            start_time=datetime.datetime.now() if start_time is None else start_time,
+            end_time=end_time
+        )
+        db.add(task)
+        db.commit()
+        logging.info('task created.')
+
+def get_all_tasks() -> list[Type[Task]]:
+    with Session(autoflush=False, bind=engine) as db:
+        logging.info('get all tasks.')
+        return db.query(Task).all()
+
+def get_all_open_tasks() -> list[Type[Task]]:
+    with Session(autoflush=False, bind=engine) as db:
+        logging.info('get all open tasks.')
+        return db.query(Task).filter(Task.done == False).all()
+
+def get_task(id: int) -> Optional[Type[Task]]:
+    with Session(autoflush=False, bind=engine) as db:
+        logging.info(f'get task {id}.')
+        return db.get(Task, id)
+
+def update_task(id: int, title: str = None, description: str = None, done: bool = None, start_time: datetime.datetime = None, end_time: datetime.datetime = None):
+    with Session(autoflush=False, bind=engine) as db:
+        task = db.get(Task, id)
+        if task:
+            task.title = title if title is not None else task.title
+            task.description = description if description is not None else task.description
+            task.done = done if done is not None else task.done
+            task.start_time = start_time if start_time is not None else task.start_time
+            task.end_time = end_time if end_time is not None else task.end_time
+            db.commit()
+            logging.info(f'task {id} updated.')
+        else:
+            logging.warning(f'task {id} not found.')
+
+def delete_task(id: int):
+    with Session(autoflush=False, bind=engine) as db:
+        task = db.get(Task, id)
+        if task:
+            db.delete(task)
+            db.commit()
+            logging.info(f'task {id} deleted.')
+        else:
+            logging.warning(f'task {id} not found.')
